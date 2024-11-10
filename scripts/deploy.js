642044861a4d0a86b58b3d7565081528ce2d53b5
Florence Ndemi <ndemiflorence@gmail.com>
2024-11-10 19:05:27 +0300

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,25 @@
+const hre = require("hardhat");
+
+async function main() {
+
+    await hre.run('compile');
+
+    const Todo = await hre.ethers.getContractFactory("TodoManager");
+
+    const todo = await Todo.deploy();
+
+    await todo.waitForDeployment();
+
+    console.log("Todo Manager Contract Address", await todo.getAddress())
+
+}
+
+main().then(() => process.exit(0))
+
+.catch((error) => { 
+
+    console.error(error);
+
+    process.exit(1);
+
+    });
